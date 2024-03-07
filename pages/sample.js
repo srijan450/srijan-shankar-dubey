963e74bc7cc9e0b5c8c2ce36380cfc0994db884a
srijan shankar dubey <srijanshankardubey99@gmail.com>
2024-03-08 00:24:46 +0530

    fixed sample export issue

--- pages/sample.js
+++ pages/sample.js
@@ -5,8 +5,9 @@
 
 // const inter = Inter({ subsets: ["latin"] });
 
-// export default function Home() {
-//   return (
+export default function Home() {
+  return <></>;
+}
 //     <>
 //       <Head>
 //         <title>Create Next App</title>
